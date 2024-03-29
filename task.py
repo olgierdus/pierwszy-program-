dec0ebfdd92b8fcfc167573a1ff7690ad33e6c43
olgierdus <163308961+olgierdus@users.noreply.github.com>
2024-03-29 17:55:45 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -1,1 +1,68 @@
-print ("My name is Olgierd")
+"Zadanie 1"
+my_age = 21
+print(my_age)
+
+pi_number = 3.14159
+print(pi_number)
+
+name_drink = 'MOnster'
+print(name_drink)
+
+I_am_a_human = True
+print(I_am_a_human)
+
+"Zadanie 2"
+
+foo = True
+bar = False
+
+check =foo and bar
+print(check)
+
+"Zadanie 3"
+
+add1 = 35
+add2 = 45
+result = add1 + add2
+print(result)
+
+"Zadanie 4"
+
+a1 = 25
+a2 = 10
+sum_result = a1 + a2
+print(sum_result)
+sum_value = 35
+
+a1 / a2
+print(a1 / a2)
+quotus = 2.5
+
+"Zadanie 5"
+
+animals = []
+print(animals)
+animals.append('Maleństwo')
+print(animals)
+animals.append('kubuś puchatek')
+print(animals)
+animals.append('Tygrysek')
+print(animals)
+
+"Zadanie 6"
+
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8"]
+print(numbers)
+print(numbers[7])
+
+"Zadanie 7"
+
+characters = ["Harry", "Ron", "Hermione"]
+print(characters)
+print(characters[-1])
+print(characters[-3])
+
+
+
+
+
